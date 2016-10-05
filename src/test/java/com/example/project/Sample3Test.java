be3a32f3893d450ab825d2e4d3a98c6f76e0ee9a
arasio <arasio.plus@gmail.com>
2016-10-05 12:29:51 +0000

    first commit

---
+++ src/test/java/com/example/project/Sample3Test.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Sample3Test {
+	@Test
+	public void test() throws InterruptedException {
+		assertEquals(3, 1 + 2);
+		Thread.sleep(3000);
+	}
+}
